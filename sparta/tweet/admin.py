d2b39be050012545613c4e50df91a2330271d55d
mynameis-sohee <eng.sohee@gmail.com>
2021-07-20 00:02:29 +0900

    회원가입/로그인/로그아웃 시  알람 설정 추가

--- sparta/tweet/admin.py
+++
@@ -1,7 +0,0 @@
-# admin 페이지에 생성한 TweetModel 넣기
-# 데이터 수정 및 추가 삭제할 때 이 admin 페이지를 이용하면 된다.
-from django.contrib import admin
-from .models import TweetModel
-
-# Register your models here.
-admin.site.register(TweetModel)
